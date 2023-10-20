87b8822a61efe1bcccf0259fe2d79f785ebadf54
Shaojieee <shaojieee@gmail.com>
2023-10-20 22:19:28 +0800

    Cerated class for a faculty profile

---
+++ objects/profile.py
@@ -0,0 +1,19 @@
+
+
+class Profile():
+    def __init__(self):
+        self.name = None
+        self.name_card = None
+        self.email = None
+        self.designation = None
+
+        self.google_scholar = None
+        self.orcid = None
+
+        self.biography = None
+        self.keywords = None
+        self.articles = None 
+        self.conference = None 
+        self.courses_taught = None 
+    
+    
