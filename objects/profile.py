6eb60c843cd9ff74c1aeb8e8566d3e8e6a3d689f
Shaojieee <shaojieee@gmail.com>
2023-10-21 11:13:25 +0800

    Create simple profile class

--- objects/profile.py
+++ objects/profile.py
@@ -2,18 +2,22 @@
 
 class Profile():
     def __init__(self):
-        self.name = None
+        self.full_name = None
         self.name_card = None
         self.email = None
         self.designation = None
 
         self.google_scholar = None
         self.orcid = None
+        self.dr_ntu = None 
 
         self.biography = None
         self.keywords = None
         self.articles = None 
         self.conference = None 
-        self.courses_taught = None 
+        self.courses_taught = None
+
+    def get_profile(self):
+        return  {key: value for key, value in self.__dict__.items() if not key.startswith('__')}
     
     
