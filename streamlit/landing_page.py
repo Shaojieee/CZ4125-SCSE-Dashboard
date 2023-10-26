1b7ddc588f56fdd5cbd40c99adcd025392045057
Shaojieee <shaojieee@gmail.com>
2023-10-26 15:48:23 +0800

    Cleaning h-index calculations

--- streamlit/landing_page.py
+++ streamlit/landing_page.py
@@ -3,6 +3,7 @@ import os
 import json
 import pandas as pd
 import datetime
+from streamlit_echarts import st_echarts
 
 
 @st.cache_data
@@ -34,14 +35,15 @@ name_selected = st.selectbox(
 
 profile = get_profile(name_selected)
 
-st.write(profile)
+# st.write(profile)
+
 
 
 row0_photo, row0_info = st.columns(
     (2,5)
 )
 
-row0_photo.title('Photo')
+row0_photo.image(f"./data_sources/{profile['image_path'][2:]}")
 
 row0_info.write(f"""{profile['full_name']}  \n  {profile['designation']}  \n {profile['email']}""")
 row0_info.link_button('Google Scholar', url=profile['google_scholar'])
@@ -54,41 +56,43 @@ row0_info.write(f"Interests: {profile['interests']}")
 
 row1= st.columns(2)
 
+total_publications = sum(profile['published_by_year'].values())
+total_citations = sum(profile['citations_by_year']['# of Citations'])
+
 row1[0].metric(
     label='\# of Publications', 
-    value=sum(profile['num_published'].values()),
-    delta=f"{profile['num_published'][str(datetime.datetime.now().year)]} YTD"
+    value=sum(profile['published_by_year'].values()),
+    delta=f"{profile['published_by_year'][str(datetime.datetime.now().year)]} YTD"
 )
 
 row1[1].metric(
     label='\# of Citations', 
-    value=sum(profile['citation_statistics']['chart'].values()),
-    delta=f"{profile['citation_statistics']['chart'][str(datetime.datetime.now().year)]} YTD"
+    value=total_citations,
+    delta=f"{profile['citations_by_year']['# of Citations'][profile['citations_by_year']['Year'].index(datetime.datetime.now().year)]} YTD"
 )
-
 
 
 row2 = st.columns(4)
 
 row2[0].metric(
     label='All Time h-index', 
-    value=profile['citation_statistics']['table']['All'][1]
+    value=profile['all_time_h_index']
 )
 
 row2[1].metric(
     label='All Time i10-index', 
-    value=profile['citation_statistics']['table']['All'][2]
+    value=profile['all_time_i10_index']
 )
 
 row2[2].metric(
-    label='Time Weight h-index', 
-    value=profile['citation_statistics']['table']['All'][2],
-    help='Exponentially Weighted h-index'
+    label='Avg Citation per publication', 
+    value=round(total_citations / total_publications, 2),
+    help='\# of Citations / # of Publications'
 )
 
 row2[3].metric(
     label='Individualised h-index', 
-    value=profile['citation_statistics']['table']['All'][2],
+    value='NA',
     help='h-index among all SCSE Faculty'
 )
 
@@ -106,34 +110,105 @@ row3[2].write('PhD')
 row3[2].write('--Not Available--' if profile['phd'] is None else profile['phd'])
 
 
-num_published = pd.DataFrame({'Year': profile['num_published'].keys(), '# of Publications':profile['num_published'].values()})
+num_published = pd.DataFrame({'Year': profile['published_by_year'].keys(), '# of Publications':profile['published_by_year'].values()})
 st.subheader('# of Publications')
 st.bar_chart(data=num_published, x='Year', y='# of Publications')
 
 
-years_citations = list(profile['citation_statistics']['chart'].keys())
-num_citations = [profile['citation_statistics']['chart'][x] for x in years_citations]
-num_citations = pd.DataFrame({'Year': years_citations, '# of Citations':num_citations})
+num_citations = pd.DataFrame(profile['citations_by_year'])
 st.subheader('# of Citations')
 st.bar_chart(data=num_citations, x='Year', y='# of Citations')
 
-st.subheader('h-index by over time')
-years_citations = list(profile['h-index_over_time'].keys())
-num_citations = [profile['h-index_over_time'][x] for x in years_citations]
+#TODO: change processing format
+st.subheader('h-index by Year')
+years_citations = list(profile['h_index_by_year'].keys())
+num_citations = [profile['h_index_by_year'][x] for x in years_citations]
 num_citations = pd.DataFrame({'Year': years_citations, 'h-index':num_citations})
 st.bar_chart(data=num_citations, x='Year', y='h-index')
 
-
+#TODO: Change processing format
 st.subheader('h-index by Publication Year')
 years_citations = list(profile['h_index_by_publication_year'].keys())
 num_citations = [profile['h_index_by_publication_year'][x] for x in years_citations]
 num_citations = pd.DataFrame({'Publication Year': years_citations, 'h-index':num_citations})
 st.bar_chart(data=num_citations, x='Publication Year', y='h-index')
 
-st.subheader('h-index by years from Publication Year')
+
+h_index = profile['h_index_by_years_from_publication_year']
+sorted_index = sorted(range(len(h_index['Publication Year'])), key=lambda index: h_index['Publication Year'][index])
+publication_year = [h_index['Publication Year'][i] for i in sorted_index]
+data__ = [h_index['h-index'][i] for i in sorted_index]
+
+st.subheader('h-index by Year grouped by Publication Year')
+
+@st.cache_data
+def generate_graph_options(start, end):
+    start_index, end_index = publication_year.index(start), publication_year.index(end)
+    options = {
+        "tooltip": {"trigger": "axis"},
+        "legend": {
+            "data": publication_year[start_index: end_index+1],
+            "selectedMode": False
+        },
+        "grid": {"left": "3%", "right": "4%", "bottom": "5%", "containLabel": True},
+        "toolbox": {"feature": {"saveAsImage": {}}},
+        "xAxis": {
+            "type": "category",
+            "boundaryGap": False,
+            "data": h_index['Year'],
+            "name": "Year",
+            "nameLocation": "middle",
+            "nameTextStyle":{
+                "padding": [10,0,0,0],
+                "fontSize": 13,
+                "color": "rgb(250, 250, 250)"
+            }
+        },
+        # TODO: Change style
+        "yAxis": {
+            "type": "value", 
+            "name": 'h-index',
+            "nameLocation": "middle",
+            "nameTextStyle":{
+                "padding": [0,0,10,0],
+                "fontSize": 13,
+                "color": "rgb(250, 250, 250)"
+            },
+            "splitLine": {
+                "lineStyle": {
+                    "color": "rgb(250,250,250)"
+                }
+            }
+        },
+        "series": [
+            {
+                "name": publication_year,
+                "type": "line",
+                # "stack": "总量",
+                "data": data
+            }
+            for publication_year, data in zip(publication_year[start_index: end_index+1], data__[start_index: end_index+1])
+        ]
+    }
+    return options
+
+
+values = st.slider(
+    'Select the range of h-index to view',
+    min_value=publication_year[0], 
+    max_value=publication_year[-1], 
+    value=(publication_year[-5], publication_year[-1]),
+    step=1
+)
+
+options = generate_graph_options(values[0], values[1])
+st_echarts(options=options, height="400px")
+
+
+
+
 
 st.subheader('Collaboration Network')
-
 
 st.subheader('Research Focus')
 
